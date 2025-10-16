3367f0ef0bdff01cb7edc94ce01b3493c872c75f
Luis Enrique Galvan Morales <lusen121502@gmail.com>
2025-10-15 22:19:07 -0500

    Añadir DuenoViewSet y VehiculoViewSet con filtros y búsqueda

--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Dueno, Vehiculo
+from .serializers import DuenoSerializer, VehiculoSerializer
 
-# Create your views here.
+class DuenoViewSet(viewsets.ModelViewSet):
+
+    queryset = Dueno.objects.all()
+    serializer_class = DuenoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nombre', 'numero_licencia']
+    ordering_fields = ['nombre', 'numero_licencia']
+
+
+class VehiculoViewSet(viewsets.ModelViewSet):
+
+    queryset = Vehiculo.objects.select_related('dueno').all()
+    serializer_class = VehiculoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['marca', 'modelo', 'anio', 'dueno__nombre']
+    filterset_fields = ['marca', 'modelo', 'anio', 'dueno']
+    ordering_fields = ['creado_en', 'marca', 'anio']
